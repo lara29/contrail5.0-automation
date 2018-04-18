ae689304778460916123e230de39c8ccb2487f4b
Lakshmi Rajan <lara29.er@gmail.com>
2018-04-17 22:41:55 -0400

    Update Contrail-Install.sh

--- Contrail-Install.sh
+++ Contrail-Install.sh
@@ -1,135 +1,204 @@
-#!/bin/bash
+!/bin/bash
 # OPENSTACK 10 WITH CONTRAIL 4.1 INSTALLATION USING SERVER-MANAGER
 # Comannd example ./Contrail-Install.sh
-# Authors: Lakshmi Rajan <lrajan@juniper.net> , Sudhishna Sendhilvelan <ssendhil@juniper.net>
-# Date: April 04, 2018
+# Authors: Sudhishna Sendhilvelan <ssendhil@juniper.net>, Lakshmi Rajan <lrajan@juniper.net>
+# Date written 2018 March 9
+
+HOME_DIR=/root/
+INFO_PATH=$HOME_DIR/Contrail_Automation/setup-info.txt
+DATA_PATH=$HOME_DIR/Contrail_Automation/contrail-host-data.txt
+BAS_INFO_PATH=$HOME_DIR/Contrail_Automation/Info.txt
+cp $HOME_DIR/BuildAutomationSystem/Info.txt $HOME_DIR/Contrail_Automation/
+echo "" > $DATA_PATH
+
+temp_ip=`awk 'NR==1' $BAS_INFO_PATH`
+temp_fsp=`awk 'NR==2' $BAS_INFO_PATH`
+if [[ -z "$temp_ip" ]]; then
+   ip=`grep "targetip" $INFO_PATH | awk -F' ' '{print $2}'`
+else
+   ip=$temp_ip
+fi
+
+if [[ -z "$temp_fsp" ]]; then
+   file_server=`grep "fileserverip" $INFO_PATH | awk -F' ' '{print $2}'`
+else
+   file_server=$temp_fsp
+fi
+echo "" > $DATA_PATH
 
 echo ""
-echo "PROVIDE HOST VM DETAILS:"
+echo " **************************************************"
+echo "      CONTRAIL HA-WEBSERVER DEPLOYMENT PROCESS"
+echo " **************************************************"
 echo ""
-echo "###################################################"
-echo "You can fetch the HOST VM details from the "
-echo "destinaton vm using the command 'ifconfig'."
-echo "To accept default values in the bracket click enter."
-echo "###################################################"
+miface=`grep "mgmt-iface" $INFO_PATH | awk -F' ' '{print $2}'`
+
+while true; do
+  echo ""
+  echo "FILE SERVER"
+  echo " IP Address: $file_server"
+  echo "CONTRAIL HOST"
+  echo " IP Address: $ip"
+  echo " Management Iface Name: $miface"
+  echo "***********************************"
+  echo ""
+  read -p 'Confirm above details (Y?N) ? ' choice
+  case $choice in
+        [Yy]* ) break;;
+        [Nn]* )
+          echo ""
+          echo "Enter new values, or press enter"
+          echo "to accept default values"
+          echo "***********************************"
+          read -p "Enter Management Interface Name ($miface): " tempiface
+          miface=${tempiface:-$miface}
+          read -p "Enter Contrail Host Address ($ip): " tempip
+          ip=${tempip:-$ip}
+          read -p "Enter File Server Ip ($file_server): " tfs
+          file_server=${tfs:-$file_server}
+          clear
+          ;;
+        * ) echo "Please answer y or n";;
+    esac
+done
+
+# Write the ip addresses into the inventory file used by Ansible
+IFS='/' read -r -a vm_ip <<< "$ip"
+IFS='/' read -r -a file_ip <<< "$file_server"
+
+echo "[local]
+localhost ansible_connection=local
+
+[contrail-ubuntu-vm]
+${vm_ip[0]}
+
+[contrail-file-server]
+${file_ip[0]}
+" > /root/Contrail_Automation/Contrail-Install/all.inv
+
+#Fetch necessary info from the target host
 echo ""
-read -p 'Enter Hostname (ubuntu): ' hostname
-hostname=${hostname:-ubuntu-lara}
-
-read -p 'Enter Ubuntu Version (xenial): ' ubuntu_version
-ubuntu_version=${ubuntu_version:-xenial}
-
-read -p 'Enter Password (juniper123): ' password
-password=${password:-juniper123}
-
-read -p 'Enter Management Interface (eno3): ' management_interface
-management_interface=${management_interface:-eno3}
-
-read -p 'Enter Mac Address (14:18:77:46:05:67): ' mac_address
-mac_address=${mac_address:-14:18:77:46:05:67}
-
-read -p 'Enter IP Address (10.10.7.205/20): ' ip_address
-ip_address=${ip_address:-10.10.7.150/20}
-
-read -p 'Enter Default Gateway (10.10.10.1): ' default_gateway
-default_gateway=${default_gateway:-10.10.10.1}
-
+echo "Fetching info from Contrail host..."
 echo ""
-while
-    read -p 'Confirm the details (Y/n): ' answer
+ansible-playbook -i Contrail-Install/all.inv Contrail-Install/contrail-host-facts.yaml --extra-vars "iface=$miface"
 
-    if [ "$answer" = "n" ] || [ "$answer" = "N" ] || [ "$answer" = "no" ] || [ "$answer" = "No" ] || [ "$answer" = "NO" ]
-    then
-        exit 1
-    elif [ "$answer" = "y" ] || [ "$answer" = "Y" ] || [ "$answer" = "yes" ] || [ "$answer" = "Yes" ] || [ "$answer" = "YES" ]
-    then
-        break
-    fi
-do :;  done
+hostname=`grep "hostname" $DATA_PATH | awk -F' ' '{print $2}'`
+ip=`grep "ip" $DATA_PATH | awk -F' ' '{print $2}'`
+mac=`grep "mac" $DATA_PATH | awk -F' ' '{print $2}'`
+gw=`grep "gw" $DATA_PATH | awk -F' ' '{print $2}'`
+iface=`grep "iface" $DATA_PATH | awk -F' ' '{print $2}'`
 
-echo "---------------------------------------------------"
+# Hardcoding values that may not change with deployment
+cluster_id=dc135
+ubuntu_version=xenial
+contrail_version=4.1.0.0-8
+openstack_version=ocata
+openstack_release=4.0.0
+echo "ubuntu-version $ubuntu_version" >> $DATA_PATH
+echo "contrail-version $contrail_version" >> $DATA_PATH
+echo "openstack-version $openstack_version" >> $DATA_PATH
+echo "openstack-release $openstack_release" >> $DATA_PATH
+echo "cluster-id $cluster_id" >> $DATA_PATH
+
+while true; do
+  echo ""
+  echo ""
+  echo " ********************************************"
+  echo "           TARGET MACHINE DETAILS"
+  echo " ********************************************"
+  echo ""
+  echo " * HOSTNAME          : $hostname"
+  echo ""
+  echo " * MGMT IFACE        : $miface"
+  echo ""
+  echo " * IP ADDRESS/CIDR   : $ip"
+  echo ""
+  echo " * GATEWAY           : $gw"
+  echo ""
+  echo " * MAC ADDRESS       : $mac"
+  echo ""
+  echo " * UBUNTU OS VERSION : $ubuntu_version "
+  echo ""
+  echo ""
+  echo " ********************************************"
+  echo "           CONTRAIL SETUP DETAILS"
+  echo " ********************************************"
+  echo ""
+  echo " * CLUSTER ID        : $cluster_id"
+  echo ""
+  echo " * CONTRAIL VERSION  : $contrail_version"
+  echo ""
+  echo " * OPENSTACK SKU     : $openstack_version"
+  echo ""
+  echo " * OPENSTACK RELEASE : $openstack_release"
+  echo ""
+  echo " * FILE SERVER       : $file_server"
+  echo ""
+  echo " ********************************************"
+
+  read -p ' Confirm above details (Y?N) ? ' choice
+  case $choice in
+  [Yy]* ) break;;
+        [Nn]* )
+          echo ""
+          echo ""
+          echo "Enter new values, or press enter to accept default values"
+          echo "********************************************************"
+          echo "TARGET MACHINE DETAILS: "
+          read -p " Enter Hostname ($hostname): " temp
+          hostname=${temp:-$hostname}
+          read -p " Enter Default Gateway ($gw): " temp
+          gw=${temp:-$gw}
+          read -p " Enter Mac Address ($mac): " temp
+          mac=${temp:-$mac}
+          read -p " Enter Ubuntu Version ($ubuntu_version): " temp
+          ubuntu_version=${temp:-$ubuntu_version}
+          echo "SETUP DETAILS: "
+          read -p " Enter cluster id ($cluster_id): " temp
+          cluster_id=${temp:-$cluster_id}
+          read -p " Enter Contrail Version ($contrail_version): " temp
+          contrail_version=${temp:-$contrail_version}
+          read -p " Enter Openstack SKU ($openstack_version): " temp
+          openstack_version=${temp:-$openstack_version}
+          read -p " Enter openstack_release ($openstack_release): " temp
+          openstack_release=${temp:-$openstack_release}
+          clear
+          ;;
+        * ) echo "Please answer y or n";;
+    esac
+done
+
+while true; do
 echo ""
-echo "PROVIDE CONTRAIL PACKAGE DETAILS:"
+echo " ********************************************"
 echo ""
-echo "###################################################"
-echo "To accept default values in the bracket click enter."
-echo "###################################################"
-echo ""
-read -p 'Enter cluster id (dc135): ' id
-id=${id:-dc135}
+read -p ' PROCEED WITH THE CONTRAIL SETUP?? (Y/n) ' choice
 
-read -p 'Enter Contrail Version (4.1.0.0-8): ' contrail_version
-contrail_version=${contrail_version:-4.1.0.0-8}
-
-read -p 'Enter Package SKU (ocata): ' package_sku
-package_sku=${package_sku:-ocata}
-
-read -p 'Enter openstack_release (4.0.0): ' openstack_release
-openstack_release=${openstack_release:-4.0.0}
-
-read -p 'Enter FILE SERVER IP (10.10.7.202): ' file_server
-file_server=${file_server:-10.10.7.202}
-
-echo ""
-
-while
-    read -p 'Confirm the details (Y/n): ' answer
-
-    if [ "$answer" = "n" ] || [ "$answer" = "N" ] || [ "$answer" = "no" ] || [ "$answer" = "No" ] || [ "$answer" = "NO" ]
-    then
-        exit 1
-    elif [ "$answer" = "y" ] || [ "$answer" = "Y" ] || [ "$answer" = "yes" ] || [ "$answer" = "Yes" ] || [ "$answer" = "YES" ]
-    then
-        break
-    fi
-do :;  done
-
-echo ""
-
-while
-    read -p 'START WITH THE CONTRAIL SETUP?? (Y/n): ' answer
-
-    if [ "$answer" = "n" ] || [ "$answer" = "N" ] || [ "$answer" = "no" ] || [ "$answer" = "No" ] || [ "$answer" = "NO" ]
-    then
-        exit 1
-    elif [ "$answer" = "y" ] || [ "$answer" = "Y" ] || [ "$answer" = "yes" ] || [ "$answer" = "Yes" ] || [ "$answer" = "YES" ]
-    then
-        break
-    fi
-do :;  done
+  case $choice in
+        [Yy]* ) break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer y or n";;
+    esac
+done
 
 echo ""
 
 echo "contrail_package:
   -
-    id: '$id'
+    id: '$cluster_id'
     contrail_version: '$contrail_version'
-    package_sku: '$package_sku'
+    package_sku: '$openstack_version'
     openstack_release: '$openstack_release'
     file_server: '$file_server'
 host_vm:
   -
     hostname: '$hostname'
     ubuntu_version: '$ubuntu_version'
-    password: '$password'
-    management_interface: '$management_interface'
-    mac_address: '$mac_address'
-    ip_address: '$ip_address'
-    default_gateway: '$default_gateway'
+    mac_address: '$mac'
+    ip_address: '$ip'
+    default_gateway: '$gw'
+    management_interface: '$miface'
 " > /root/Contrail_Automation/Contrail-Install/vars/contrail.info
-
-IFS='/' read -r -a vm_ip <<< "$ip_address"
-IFS='/' read -r -a file_ip <<< "$file_server"
-
-echo "[local]
-localhost ansible_connection=local
-
-[contrail-ubuntu-host]
-${vm_ip[0]}
-
-[contrail-file-server]
-${file_ip[0]}
-" > /root/Contrail_Automation/Contrail-Install/all.inv
 
 echo ""
 echo ""
